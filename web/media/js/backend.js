a3ea805a70129b07bdafd8215d766c656ab53333
Skaiver <xm3lte@gmail.com>
2022-12-09 13:09:44 +0100

    feat(backend): continued backend frontend

--- web/media/js/backend.js
+++ web/media/js/backend.js
@@ -4,12 +4,26 @@ new ApiHelper().readAllProducts((data) => {
     let response = JSON.parse(data);
     let products = response.response;
     console.table(products);
-    document.querySelector('#table_products').innerHTML = data;
-    
 
+    let table = document.querySelector('#table_products');
+   
+    
+    let table_body = table.querySelector('tbody');
     let template_table_product_entry = document.querySelector('template#productEntryTemplate');
-    for(product in products){
-        
+    let clon;
+    for(let product of products){
+        clon = template_table_product_entry.content.cloneNode(true);
+
+        console.log(clon);
+        console.log(clon.querySelector('[data-backend-product_id]'));
+
+        /* set product values */
+        clon.querySelector('[data-backend-product_id]').textContent = product.id;
+        clon.querySelector('[data-backend-product_title]').innerHTML = product.title;
+        clon.querySelector('[data-backend-product_description]').innerHTML = product.description;
+        clon.querySelector('[data-backend-product_price]').innerHTML = product.price;
+
+        table_body.appendChild(clon);
     }
 
 });
